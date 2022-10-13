5e8291a7df1738bb4ebddfe906f2c7062133c92e
htuohz <htuohz@gmail.com>
2022-10-14 09:07:45 +1100

    moved tests files

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from "@testing-library/react";
+import App from "../App";
+import { renderWithProviders } from "./test-utils";
+
+test("renders app", () => {
+  renderWithProviders(<App />);
+  const currentDate = new Date().toLocaleDateString("en-AU");
+  const currentDateComponent = screen.getByRole("heading", {
+    name: currentDate,
+  });
+  expect(currentDateComponent).toBeInTheDocument();
+  const switches = screen.getAllByRole("radio");
+  expect(switches.length).toBe(2);
+});
